c078b6bc73187ba1cfa24b3dbcf7979a6d17e9c2
Naushad Pasha Puliyambalath <naushadpasha@gmail.com>
2020-04-10 15:36:58 -0400

    website updated.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(name='haziris',
-      version='0.4.0',
+      version='0.5.0',
       description='HTML and JS Visualization templates',
       url='http://github.com/haziris',
       author='Naushad Pasha Puliyambalath',
-      author_email='pasha@vizjs.org',
+      author_email='pasha@haziris.org',
       license='MIT',
       include_package_data=True,
       packages=['haziris'],
